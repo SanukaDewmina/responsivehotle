43a29c56a74227693b8ce50d4dc346db52e7b86a
SanukaDewmina <161559862+SanukaDewmina@users.noreply.github.com>
2024-05-06 17:17:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const responsiveNavEl = document.getElementById("responsnavbarsection");
+
+
+function navbaronoff(){
+  responsiveNavEl.classList.toggle("active");
+}
+function navclose(){
+  responsiveNavEl.classList.toggle("active");
+}
+function navBtnclose(){
+  responsiveNavEl.classList.toggle("active");
+}
+// cart window section java script code
+
+const cartWindow = document.getElementById("booking-card");
+
+
+function navBtnclose(){
+  cartWindow.classList.toggle("active");
+  responsiveNavEl.classList.toggle("active");
+
+}
+function windowclose(){
+  cartWindow.classList.toggle("active");
+}
+function opennavwindwo2time(){
+  cartWindow.classList.toggle("active");
+}
